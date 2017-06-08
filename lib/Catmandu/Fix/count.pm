22a19540ff35d2952aea37b475f428747f29132f
Nicolas Steenlant <nicolas.steenlant@ugent.be>
2017-06-08 14:59:32 +0300

    count, downcase builder versions

--- lib/Catmandu/Fix/count.pm
+++ lib/Catmandu/Fix/count.pm
@@ -5,7 +5,7 @@ use Catmandu::Sane;
 our $VERSION = '1.0507';
 
 use Moo;
-use Types::Standard qw(ArrayRef HashRef);
+use Catmandu::Util qw(is_array_ref is_hash_ref);
 use namespace::clean;
 use Catmandu::Fix::Has;
 
@@ -17,11 +17,11 @@ sub BUILD {
     my ($self) = @_;
 
     my $builder = $self->at($self->path);
-    $builder->if(ArrayRef)->set(sub {
+    $builder->if(\&is_array_ref)->set(sub {
         my $val = $_[0];
         scalar(@$val);
     });
-    $builder->if(HashRef)->set(sub {
+    $builder->if(\&is_hash_ref)->set(sub {
         my $val = $_[0];
         scalar(keys %$val);
     });
