6b3c7b6aa4741838d815a82b97b569d93cc44b0e
Nicolas Steenlant <nicolas.steenlant@ugent.be>
2017-06-30 14:17:39 +0200

    flatten builder version

--- lib/Catmandu/Fix/flatten.pm
+++ lib/Catmandu/Fix/flatten.pm
@@ -4,19 +4,27 @@ use Catmandu::Sane;
 
 our $VERSION = '1.0602';
 
+use Catmandu::Util qw(is_array_ref);
 use Moo;
 use namespace::clean;
 use Catmandu::Fix::Has;
 
 has path => (fix_arg => 1);
 
-with 'Catmandu::Fix::SimpleGetValue';
+with 'Catmandu::Fix::Base';
 
-sub emit_value {
-    my ($self, $var) = @_;
-    "if (is_array_ref(${var})) {"
-        . "${var} = [map { ref \$_ eq 'ARRAY' ? \@\$_ : \$_ } \@{${var}}] "
-        . "while grep ref \$_ eq 'ARRAY', \@{${var}};" . "}";
+sub BUILD {
+    my ($self) = @_;
+
+    my $builder = $self->builder;
+    $builder->get($self->path)->update(
+        sub {
+            my $val = $_[0];
+            return $builder->cancel unless is_array_ref($val);
+            $val = [map { is_array_ref($_) ? @$_ : $_ } @$val] while grep { is_array_ref($_) } @$val;
+            $val;
+        }
+    );
 }
 
 1;
